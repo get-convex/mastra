279a5039b379fd21a07c47d1f87bfdcbcc995d59
Ian Macartney <ianhmacartney@gmail.com>
2025-03-28 06:15:46 -0700

    undo hack b/c convex v1.21 fixes it

--- src/component/_generated/api.js
+++ src/component/_generated/api.js
@@ -20,8 +20,4 @@ import { anyApi, componentsGeneric } from "convex/server";
  */
 export const api = anyApi;
 export const internal = anyApi;
-
-/**
- * @type {ReturnType<typeof import('convex/server').componentsGeneric>}
- */
 export const components = componentsGeneric();
