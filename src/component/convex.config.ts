b4bee9d73e0d7ac9ceebe59741109a00b9aa7d18
Ian Macartney <ianhmacartney@gmail.com>
2025-04-01 19:56:07 -0700

    remove workpool dependency

--- src/component/convex.config.ts
+++ src/component/convex.config.ts
@@ -1,7 +1,5 @@
 import { defineComponent } from "convex/server";
-import workpool from "@convex-dev/workpool/convex.config";
 
 const component = defineComponent("mastra");
-component.use(workpool);
 
 export default component;
